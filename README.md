3df64ccecfedd98d9bbf9b0d1b12558b8bf2683c
jasonwynn10 <jasonwynn10@me.com>
2022-06-05 07:44:54 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# libCustomPack
+
+[![Poggit-Ci](https://poggit.pmmp.io/ci.shield/jasonwynn10/libCustomPack/libCustomPack)](https://poggit.pmmp.io/ci/jasonwynn10/libCustomPack/libCustomPack)
+
+A small library for building and registering resource packs with PocketMine-MP
+## Usage
+This viron was made for developers to build resource packs from plugin resources or from files on the disk, and allows adding new resource packs to the stack without requiring a server restart.
+*NOTE*: New resource packs are not automatically updated for previously loaded players. Only new players will see new resource packs.
+
+#### Required imports
+The following imports are necessary to use the virion library:
+```php
+use libCustomPack\libCustomPack;
+```
+
+### API
+#### Building a resource pack
+2 methods are added which allow build a resource pack using the plugin's resource directory or any other given path.
+```php
+libCustomPack::generatePackFromResources($plugin);
+//OR
+libCustomPack::generatePackFromPath($plugin, $MyFullFolderPath);
+```
+#### Registering a resource pack
+A resource pack can be added to the resource stack using the `registerResourcePack()` method.
+```php
+libCustomPack::registerResourcePack($resoucePackInstance);
+```
+#### Unregistering a resource pack
+During a plugin's onDisable() method, it is recommended to call the `unregisterResourcePack()` method to be removed from the resource stack.
+```php
+libCustomPack::unregisterResourcePack($resoucePackInstance);
+```
+
+### SubFolders
+If the resource pack is a folder we don't know the exact location of within the given folder, we can use the `$packFolderName` parameter to specify the name of the folder to be found.
+The default subfolder name is the plugin's name followed by " Pack".
+```php
+libCustomPack::generatePackFromResources($plugin, $packFolderName);
+//OR
+libCustomPack::generatePackFromPath($plugin, $MyFullFolderPath, $packFolderName);
+```
---
+++ src/libCustomPack/libCustomPack.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+namespace libCustomPack;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\resourcepacks\ResourcePack;
+use pocketmine\resourcepacks\ZippedResourcePack;
+use pocketmine\Server;
+use pocketmine\utils\Filesystem;
+use Webmozart\PathUtil\Path;
+
+final class libCustomPack{
+
+	final public static function generatePackFromResources(PluginBase $plugin, ?string $packFolderName = null) : ZippedResourcePack {
+		$packFolderName ??= $plugin->getName().' Pack';
+		$packFolderRegex = str_replace(' ', '\h', $packFolderName);
+
+		$zip = new \ZipArchive();
+		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName().'.mcpack');
+		$zip->open($outputFilePath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+		foreach($plugin->getResources() as $resource){
+			if($resource->isFile() and str_contains($resource->getPathname(), $packFolderName)){
+				$relativePath = Path::normalize(preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname()));
+				$plugin->saveResource(Path::join($packFolderName, $relativePath), false);
+				$zip->addFile(Path::join($plugin->getDataFolder(), $packFolderName, $relativePath), $relativePath);
+			}
+		}
+		$zip->close();
+		Filesystem::recursiveUnlink(Path::join($plugin->getDataFolder(), $packFolderName)); // clean up
+
+		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName().'.mcpack';
+		assert(!rename($outputFilePath, $newFileName)); // TODO: does the ZippedResourcePack get freed before this happens?
+
+		return new ZippedResourcePack($newFileName);
+	}
+
+	final public static function generatePackFromPath(PluginBase $plugin, string $inputFilePath, ?string $packFolderName = null) : ZippedResourcePack {
+		$packFolderName ??= $plugin->getName().' Pack';
+		$packFolderRegex = str_replace(' ', '\h', $packFolderName);
+
+		$zip = new \ZipArchive();
+		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName().'.mcpack');
+		$zip->open($outputFilePath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+		foreach(
+			new \RecursiveDirectoryIterator(
+				$inputFilePath,
+				\FilesystemIterator::KEY_AS_PATHNAME|\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS
+			) as
+			$resource
+		){
+			if($resource->isFile() and str_contains($resource->getPathname(), $packFolderName)){
+				$relativePath = Path::normalize(preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname()));
+				copy(Path::join($packFolderName, $relativePath), Path::join($plugin->getDataFolder(), $packFolderName, $relativePath));
+				$zip->addFile(Path::join($plugin->getDataFolder(), $packFolderName, $relativePath), $relativePath);
+			}
+		}
+		$zip->close();
+		Filesystem::recursiveUnlink(Path::join($plugin->getDataFolder(), $packFolderName)); // clean up
+
+		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName().'.mcpack';
+		assert(!rename($outputFilePath, $newFileName)); // TODO: does the ZippedResourcePack get freed before this happens?
+
+		return new ZippedResourcePack($newFileName);
+	}
+
+	final public static function registerResourcePack(ResourcePack $resourcePack) : void {
+		$manager = Server::getInstance()->getResourcePackManager();
+
+		$reflection = new \ReflectionClass($manager);
+
+		$property = $reflection->getProperty("resourcePacks");
+		$property->setAccessible(true);
+		$currentResourcePacks = $property->getValue($manager);
+		$currentResourcePacks[] = $resourcePack;
+		$property->setValue($manager, $currentResourcePacks);
+
+		$property = $reflection->getProperty("uuidList");
+		$property->setAccessible(true);
+		$currentUUIDPacks = $property->getValue($manager);
+		$currentUUIDPacks[mb_strtolower($resourcePack->getPackId())] = $resourcePack;
+		$property->setValue($manager, $currentUUIDPacks);
+
+		$property = $reflection->getProperty("serverForceResources");
+		$property->setAccessible(true);
+		$property->setValue($manager, true);
+	}
+
+	final public static function unregisterResourcePack(ResourcePack $resourcePack) : void {
+		$manager = Server::getInstance()->getResourcePackManager();
+
+		$reflection = new \ReflectionClass($manager);
+
+		$property = $reflection->getProperty("resourcePacks");
+		$property->setAccessible(true);
+		$currentResourcePacks = $property->getValue($manager);
+		$key = array_search($resourcePack, $currentResourcePacks);
+		if($key !== false){
+			unset($currentResourcePacks[$key]);
+			$property->setValue($manager, $currentResourcePacks);
+		}
+
+		$property = $reflection->getProperty("uuidList");
+		$property->setAccessible(true);
+		$currentUUIDPacks = $property->getValue($manager);
+		if(isset($currentResourcePacks[mb_strtolower($resourcePack->getPackId())])) {
+			unset($currentUUIDPacks[mb_strtolower($resourcePack->getPackId())]);
+			$property->setValue($manager, $currentUUIDPacks);
+		}
+	}
+
+}
