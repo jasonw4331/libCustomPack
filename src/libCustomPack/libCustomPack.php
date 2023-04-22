41d140225721c84d2f28ef2f1af6a8e1f9c76964
jasonwynn10 <jasonwynn10@me.com>
2023-04-22 15:08:02 -0400

    Virion 3.0 spec & Github Actions Tests

--- src/libCustomPack/libCustomPack.php
+++ src/libCustomPack/libCustomPack.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 namespace libCustomPack;
 
@@ -8,19 +9,32 @@ use pocketmine\resourcepacks\ZippedResourcePack;
 use pocketmine\Server;
 use pocketmine\utils\Filesystem;
 use Webmozart\PathUtil\Path;
+use function array_search;
+use function assert;
+use function copy;
+use function is_string;
+use function mb_strtolower;
+use function preg_replace;
+use function rename;
+use function str_contains;
+use function str_replace;
 
 final class libCustomPack{
 
 	final public static function generatePackFromResources(PluginBase $plugin, ?string $packFolderName = null) : ZippedResourcePack {
-		$packFolderName ??= $plugin->getName().' Pack';
+		$packFolderName ??= $plugin->getName() . ' Pack';
 		$packFolderRegex = str_replace(' ', '\h', $packFolderName);
 
 		$zip = new \ZipArchive();
-		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName().'.mcpack');
+		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName() . '.mcpack');
 		$zip->open($outputFilePath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 		foreach($plugin->getResources() as $resource){
-			if($resource->isFile() and str_contains($resource->getPathname(), $packFolderName)){
-				$relativePath = Path::normalize(preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname()));
+			if($resource->isFile() && str_contains($resource->getPathname(), $packFolderName)){
+				$found = preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname());
+				if(!is_string($found)){
+					throw new \InvalidArgumentException("Invalid path: $packFolderName");
+				}
+				$relativePath = Path::normalize($found);
 				$plugin->saveResource(Path::join($packFolderName, $relativePath), false);
 				$zip->addFile(Path::join($plugin->getDataFolder(), $packFolderName, $relativePath), $relativePath);
 			}
@@ -28,28 +42,29 @@ final class libCustomPack{
 		$zip->close();
 		Filesystem::recursiveUnlink(Path::join($plugin->getDataFolder(), $packFolderName)); // clean up
 
-		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName().'.mcpack';
+		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName() . '.mcpack';
 		assert(rename($outputFilePath, $newFileName));
 
 		return new ZippedResourcePack(Path::join($plugin->getDataFolder(), $newFileName));
 	}
 
 	final public static function generatePackFromPath(PluginBase $plugin, string $inputFilePath, ?string $packFolderName = null) : ZippedResourcePack {
-		$packFolderName ??= $plugin->getName().' Pack';
+		$packFolderName ??= $plugin->getName() . ' Pack';
 		$packFolderRegex = str_replace(' ', '\h', $packFolderName);
 
 		$zip = new \ZipArchive();
-		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName().'.mcpack');
+		$outputFilePath = Path::join($plugin->getDataFolder(), $plugin->getName() . '.mcpack');
 		$zip->open($outputFilePath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-		foreach(
-			new \RecursiveDirectoryIterator(
-				$inputFilePath,
-				\FilesystemIterator::KEY_AS_PATHNAME|\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS
-			) as
+		foreach(new \RecursiveDirectoryIterator($inputFilePath,
+			\FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::CURRENT_AS_FILEINFO | \FilesystemIterator::SKIP_DOTS) as
 			$resource
 		){
-			if($resource->isFile() and str_contains($resource->getPathname(), $packFolderName)){
-				$relativePath = Path::normalize(preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname()));
+			if($resource->isFile() && str_contains($resource->getPathname(), $packFolderName)){
+				$found = preg_replace("/.*[\/\\\\]{$packFolderRegex}[\/\\\\].*/U", '', $resource->getPathname());
+				if(!is_string($found)){
+					throw new \InvalidArgumentException("Invalid path: $packFolderName");
+				}
+				$relativePath = Path::normalize($found);
 				copy(Path::join($packFolderName, $relativePath), Path::join($plugin->getDataFolder(), $packFolderName, $relativePath));
 				$zip->addFile(Path::join($plugin->getDataFolder(), $packFolderName, $relativePath), $relativePath);
 			}
@@ -57,7 +72,7 @@ final class libCustomPack{
 		$zip->close();
 		Filesystem::recursiveUnlink(Path::join($plugin->getDataFolder(), $packFolderName)); // clean up
 
-		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName().'.mcpack';
+		$newFileName = (new ZippedResourcePack($outputFilePath))->getPackName() . '.mcpack';
 		assert(!rename($outputFilePath, $newFileName)); // TODO: does the ZippedResourcePack get freed before this happens?
 
 		return new ZippedResourcePack($newFileName);
@@ -70,12 +85,14 @@ final class libCustomPack{
 
 		$property = $reflection->getProperty("resourcePacks");
 		$property->setAccessible(true);
+		/** @var ResourcePack[] $currentResourcePacks */
 		$currentResourcePacks = $property->getValue($manager);
 		$currentResourcePacks[] = $resourcePack;
 		$property->setValue($manager, $currentResourcePacks);
 
 		$property = $reflection->getProperty("uuidList");
 		$property->setAccessible(true);
+		/** @var array<string, ResourcePack> $currentUUIDPacks */
 		$currentUUIDPacks = $property->getValue($manager);
 		$currentUUIDPacks[mb_strtolower($resourcePack->getPackId())] = $resourcePack;
 		$property->setValue($manager, $currentUUIDPacks);
@@ -92,8 +109,9 @@ final class libCustomPack{
 
 		$property = $reflection->getProperty("resourcePacks");
 		$property->setAccessible(true);
+		/** @var ResourcePack[] $currentResourcePacks */
 		$currentResourcePacks = $property->getValue($manager);
-		$key = array_search($resourcePack, $currentResourcePacks);
+		$key = array_search($resourcePack, $currentResourcePacks, true);
 		if($key !== false){
 			unset($currentResourcePacks[$key]);
 			$property->setValue($manager, $currentResourcePacks);
@@ -101,6 +119,7 @@ final class libCustomPack{
 
 		$property = $reflection->getProperty("uuidList");
 		$property->setAccessible(true);
+		/** @var array<string, ResourcePack> $currentUUIDPacks */
 		$currentUUIDPacks = $property->getValue($manager);
 		if(isset($currentResourcePacks[mb_strtolower($resourcePack->getPackId())])) {
 			unset($currentUUIDPacks[mb_strtolower($resourcePack->getPackId())]);
